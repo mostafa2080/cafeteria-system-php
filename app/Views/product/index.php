cd486a9b74241110aec920148d2a65ae914f8b75
mostafa2080 <mostafashaaban2080@gmail.com>
2023-04-29 14:19:43 +0300

    deletion modal added

--- app/Views/product/index.php
+++ app/Views/product/index.php
@@ -32,15 +32,51 @@ include 'app/Views/Layout-top.php';
                     </td>
                     <td><?php echo $product->categoryID; ?></td>
                     <td>
-                        <a href="/product/edit?id=<?php echo $product->id; ?>"><i class="fas fa-edit"></i></a>
-                        <a href="/product/delete?id=<?php echo $product->id; ?>"><i class="fas fa-trash"></i></a>
-                        <a href="/product/show?id=<?php echo $product->id; ?>"><i class="fas fa-info-circle"></i></a>
+                        <a href="/product/edit?id=<?php echo $product->id; ?>"><i class="fas fa-edit action"></i></a>
+                        <a type="button" data-toggle="modal" data-target="#deleteModal<?php echo $product->id; ?>"><i class="fas fa-trash action"></i></a>
+                        <a href="/product/show?id=<?php echo $product->id; ?>"><i class="fas fa-info-circle action"></i></a>
                     </td>
                 </tr>
+                <!-- Delete Modal -->
+                <div class="modal fade" id="deleteModal<?php echo $product->id; ?>" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel<?php echo $product->id; ?>" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="deleteModalLabel<?php echo $product->id; ?>">Confirm Deletion</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Are you sure you want to delete this product?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <a href="/product/delete?id=<?php echo $product->id; ?>" class="btn btn-danger">Delete</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             <?php endforeach; ?>
         </tbody>
     </table>
 </div>
+<style>
+   .action{
+  display: inline-block;
+  padding: 5px 10px;
+  background-color: #007bff;
+  color: #fff;
+  border-radius: 5px;
+  text-decoration: none;
+}
+
+.action:hover {
+  background-color: #0069d9;
+  color: #fff;
+}
+
+</style>
 <?php
 include 'app/Views/Layout-bot.php';
 ?>
